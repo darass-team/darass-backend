f4282ce1b357da4695d6df246b7c05904d3ce5a3
JinYoung Park <basejin0810@naver.com>
2022-01-03 00:08:16 +0900

    feat: S3 버킷을 활용하여 프로필 이미지 업로드 및 삭제 기능 구현

--- backend/src/main/java/com/darass/user/S3ClientConfig.java
+++ backend/src/main/java/com/darass/user/S3ClientConfig.java
@@ -1,17 +1,31 @@
 package com.darass.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3ClientConfig {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
     @Bean
     public S3Client s3Client() {
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
-            .region(Region.AP_NORTHEAST_2)
+            .region(Region.of(region))
+            .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
             .build();
     }
 }
---
+++ backend/src/main/java/com/darass/user/infrastructure/S3Service.java
@@ -0,0 +1,85 @@
+package com.darass.user.infrastructure;
+
+import com.darass.exception.ExceptionWithMessageAndCode;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectResponse;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Component
+@RequiredArgsConstructor
+public class S3Service {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final S3Client s3Client;
+
+    public String upload(MultipartFile multipartFile) {
+        File uploadFile = convert(multipartFile);
+        return uploadToS3(uploadFile);
+    }
+
+    public DeleteObjectResponse delete(String profileImageUrl) {
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+            .bucket(bucket)
+            .key(extractKey(profileImageUrl))
+            .build();
+        return s3Client.deleteObject(deleteObjectRequest);
+    }
+
+    private String extractKey(String profileImageUrl) {
+        if (!profileImageUrl.contains("/")) {
+            return "";
+        }
+        String[] split = profileImageUrl.split("/");
+        return URLDecoder.decode(split[split.length - 1], StandardCharsets.UTF_8);
+    }
+
+    private File convert(MultipartFile multipartFile) {
+        File file = new File(multipartFile.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(multipartFile.getBytes());
+        } catch (IOException e) {
+            throw ExceptionWithMessageAndCode.IO_EXCEPTION.getException();
+        }
+        return file;
+    }
+
+    private String uploadToS3(File uploadFile) {
+        String fileName = new Date().getTime() + uploadFile.getName();
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+            .bucket(bucket)
+            .key(fileName)
+            .build();
+        try {
+            s3Client.putObject(objectRequest, RequestBody.fromFile(uploadFile));
+        } catch (SdkClientException e) {
+            throw e;
+        } finally {
+            uploadFile.delete();
+        }
+        return makeFilePath(fileName);
+    }
+
+    private String makeFilePath(String fileName) {
+        GetUrlRequest getUrlRequest = GetUrlRequest.builder()
+            .bucket(bucket)
+            .key(fileName)
+            .build();
+        return s3Client.utilities().getUrl(getUrlRequest).toString();
+    }
+}
